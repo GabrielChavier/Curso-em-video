489a3f46e9a601c7c5c7e1a6a8c5205a855f18a3
GabrielChavier <gabrielfranco848@gmail.com>
2024-10-16 01:07:25 -0300

    commit

--- TREINAR.py
+++ TREINAR.py
@@ -1,4 +1,1 @@
-from math import radians,cos,sin,tan
-angulo = float(input('Digite o angulo: '))
-c = radians(angulo)
-print(f'O seno é igual {sin(c):.2f} o coseno é igual {cos(c):.2f} e a tangente é igual a {tan(c):.2f}')
+sera q pego
